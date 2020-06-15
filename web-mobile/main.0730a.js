d127aac1ce74590b94b97d9886c757e2951843bf
Rhett <1345583212@qq.com>
2020-06-15 16:08:58 +0800

    6.15

--- web-mobile/main.0730a.js
+++ web-mobile/main.0730a.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6fa68.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.3a570.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.39aea.js');
+        require('src/settings.d0a5d.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.39aea.js');
+        require('src/settings.d0a5d.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
