1c9400f8e1bbd2484e71317f3ba5c1b1e91a40d2
Rhett <1345583212@qq.com>
2020-06-18 11:31:59 +0800

    6.18.1

--- redEnvelope2/main.262a8.js
+++ redEnvelope2/main.262a8.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.b55fa.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.da294.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.0514b.js');
+        require('src/settings.40e98.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.0514b.js');
+        require('src/settings.40e98.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
